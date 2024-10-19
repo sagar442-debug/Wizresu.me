f5fabf88e01aceb3336ee39a9c641e5ac6dcf76d
Sagar Sapkota <67532085+sagar442-debug@users.noreply.github.com>
2024-10-19 12:05:29 -0600

    Deployment issue configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    runtime: "edge", // Enable edge capabilities for routing
+  },
   env: {
     // Add only the environment variables you need to access client-side
     GOOGLE_API_KEY: process.env.GOOGLE_API, // Assuming you have a NEXT_PUBLIC_API_KEY in .env or .env.local
