6a48527239501cbe6c5a585290c69572cb0f1e29
Sagar Sapkota <67532085+sagar442-debug@users.noreply.github.com>
2024-10-21 22:33:59 -0600

    Fixed landing page for lg device with width 1024

--- app/(components)/SecondPartLanding.jsx
+++ app/(components)/SecondPartLanding.jsx
@@ -30,8 +30,8 @@ function SecondPartLanding() {
                 <div className="p-3 rounded-full w-fit bg-gradient-to-r from-[#7EA8C4] to-[#BAC56A] border-8 border-[#fafafa] z-10 relative">
                   <PiNotepadFill className="text-5xl text-white" />
                 </div>
-                <div className="bg-[#7FA9C4] mt-2 ml-10 rounded-r-[50px] p-4 w-60 h-28 rounded-bl-[50px] rounded-tl-[5px]">
-                  <h1 className="text-lg font-semibold text-white  ">
+                <div className="bg-[#7FA9C4] mt-2 ml-10 rounded-r-[50px] p-4 xl:w-60 xl:h-28 lg:w-52 lg:h-24  rounded-bl-[50px] rounded-tl-[5px]">
+                  <h1 className="xl:text-lg lg:text-base font-semibold text-white  ">
                     Upload job description and your details
                   </h1>
                 </div>
@@ -40,8 +40,8 @@ function SecondPartLanding() {
                 <div className="p-3 rounded-full w-fit bg-gradient-to-r from-[#7EA8C4] to-[#BAC56A] border-8 border-[#fafafa] z-10 relative">
                   <MdOutlineDocumentScanner className="text-5xl text-white" />
                 </div>
-                <div className="bg-[#7FA9C4] -mt-56 ml-10 rounded-r-[50px] p-4 w-60 h-28 rounded-tl-[50px] rounded-bl-[5px]">
-                  <h1 className="text-lg font-semibold text-white ">
+                <div className="bg-[#7FA9C4]  xl:ml-10 lg:ml-14 xl:-mt-56 lg:-mt-48 rounded-r-[50px] p-4 xl:w-60 xl:h-28 lg:w-52 lg:h-24 rounded-tl-[50px] rounded-bl-[5px]">
+                  <h1 className="xl:text-lg lg:text-base font-semibold text-white ">
                     We scan the hidden gems in the rough
                   </h1>
                 </div>
@@ -50,18 +50,18 @@ function SecondPartLanding() {
                 <div className="p-3 rounded-full w-fit bg-gradient-to-r from-[#7EA8C4] to-[#BAC56A] border-8 border-[#fafafa] z-10 relative">
                   <HiOutlineSparkles className="text-5xl text-white" />
                 </div>
-                <div className="bg-[#7FA9C4] mt-2 ml-10 rounded-r-[50px] p-3 w-60 h-28 rounded-bl-[50px] rounded-tl-[5px]">
-                  <h1 className="text-lg font-semibold text-white ">
+                <div className="bg-[#7FA9C4] xl:mt-2 lg:mt-2 ml-10 rounded-r-[50px] p-3 xl:w-60 xl:h-28 lg:w-52 lg:h-24 rounded-bl-[50px] rounded-tl-[5px]">
+                  <h1 className="xl:text-lg lg:text-base font-semibold text-white ">
                     We provide exceptional Suggestions with AI
                   </h1>
                 </div>
               </div>
               <div className="download flex-col">
-                <div className="p-3 rounded-full  w-fit bg-gradient-to-r from-[#7EA8C4] to-[#BAC56A] border-8 border-[#fafafa] z-10 relative">
+                <div className="p-3 rounded-full  w-fit bg-gradient-to-r from-[#7EA8C4] to-[#BAC56A] border-8 border-[#fafafa] z-10 relative ">
                   <RiFileDownloadFill className="text-5xl text-white" />
                 </div>
-                <div className="bg-[#7FA9C4] -mt-56 ml-10 rounded-r-[50px] w-60 h-28  rounded-bl-[5px] rounded-tl-[50px]">
-                  <h1 className="text-lg font-semibold text-white p-3 ">
+                <div className="bg-[#7FA9C4]  lg:ml-16 xl:ml-10 rounded-r-[50px] xl:w-60 xl:h-28 lg:w-52 lg:h-24  rounded-bl-[5px] rounded-tl-[50px] lg:-translate-x-12 xl:-translate-x-0  xl:-mt-56 lg:-mt-48">
+                  <h1 className="xl:text-lg lg:text-base font-semibold text-white p-3 ">
                     One Click Download, One Step Closer to Your Dream Job
                   </h1>
                 </div>
@@ -69,7 +69,7 @@ function SecondPartLanding() {
             </div>
           </div>
 
-          <div className="w-[50vw] bg-[#7FA9C4] h-3 rounded-2xl absolute top-10 transform -translate-y-1/2 "></div>
+          <div className="lg:w-[80vw] xl:w-[50vw] bg-[#7FA9C4] h-3 rounded-2xl absolute top-10 transform -translate-y-1/2 "></div>
         </div>
       </div>
       <div>
