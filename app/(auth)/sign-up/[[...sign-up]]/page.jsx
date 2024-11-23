d3f013cb932045f3f6d12dd8769fba580580a737
Sagar Sapkota <67532085+sagar442-debug@users.noreply.github.com>
2024-11-22 17:08:24 -0700

    Fixed the sign in and up page displays to go back to previous page

--- app/(auth)/sign-up/[[...sign-up]]/page.jsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.jsx
@@ -1,10 +1,18 @@
 import { SignUp } from "@clerk/nextjs";
+import { ChevronLeft } from "lucide-react";
+import Link from "next/link";
 import React from "react";
 export const runtime = "edge";
 
 const page = () => {
   return (
-    <div className="flex items-center justify-center h-[100vh] ">
+    <div className="flex items-center justify-center h-[100vh] flex-col">
+      <Link className="flex w-24  pb-4 group " href={"/"}>
+        <ChevronLeft className="group-hover:text-blue-600" />
+        <span className="group-hover:underline group-hover:text-blue-600">
+          Go Back
+        </span>
+      </Link>
       <SignUp />
     </div>
   );
