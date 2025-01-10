b83ba028b30a4955b7d6f45a98436f400fff4264
Sagar Sapkota <67532085+sagar442-debug@users.noreply.github.com>
2025-01-09 19:06:27 -0700

    Adding the logo in the landing page

--- app/(components)/PhoneHeader.jsx
+++ app/(components)/PhoneHeader.jsx
@@ -1,19 +1,26 @@
 import Link from "next/link";
 import React from "react";
+import Logo from "./Logo";
 
 const PhoneHeader = () => {
   return (
-    <nav className="md:hidden flex space-x-5 justify-center items-center pb-8">
-      <Link
-        className="bg-[#2a2663] py-1 px-2 rounded font-semibold text-white"
-        href={"/sign-in"}
-      >
-        Login
-      </Link>
-      <Link className="underline" href={"/sign-up"}>
-        Sign Up
-      </Link>
-    </nav>
+    <div className="mb-8 pb-5 border-b border-[#2a26633b]">
+      <div className="flex justify-center items-center mb-6">
+        <Logo />
+        <h1 className="text-[#2a2663] font-semibold text-lg">Wizresu.me</h1>
+      </div>
+      <nav className="md:hidden flex space-x-5 justify-center items-center">
+        <Link
+          className="bg-[#2a2663] py-1 px-2 rounded font-semibold text-white"
+          href={"/sign-in"}
+        >
+          Login
+        </Link>
+        <Link className="underline" href={"/sign-up"}>
+          Sign Up
+        </Link>
+      </nav>
+    </div>
   );
 };
 
